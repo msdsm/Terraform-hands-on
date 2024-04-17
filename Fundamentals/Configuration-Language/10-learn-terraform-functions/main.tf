6896afc751cfc4a7e074c59f864c2aa4d6a6b84a
msdsm <y.msd0110@gmail.com>
2024-04-17 14:52:50 +0900

    fix

--- Fundamentals/Configuration-Language/10-learn-terraform-functions/main.tf
+++ Fundamentals/Configuration-Language/10-learn-terraform-functions/main.tf
@@ -24,13 +24,13 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+*/
 
 resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
-*/
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
